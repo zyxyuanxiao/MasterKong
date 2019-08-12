6ff0af392672071952bec6ead51736a8291400f4
ljw777 <17347116335@163.con>
2019-08-12 11:59:17 +0800

    liujiawei 首页第二版提交

--- pages/Homepage/Homepage.js
+++ pages/Homepage/Homepage.js
@@ -7,7 +7,16 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),//11111
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
   //事件处理函数
   bindViewTap: function () {
