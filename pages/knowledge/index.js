8d0e6b57af4f2d06ce620193d17f0cd75f09e2b3
ljw777 <17347116335@163.con>
2019-08-14 17:05:51 +0800

    更多代码提交

--- pages/knowledge/index.js
+++ pages/knowledge/index.js
@@ -7,7 +7,9 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    // showModalStatus: false,
+    showModal: false
   },
   //事件处理函数
   bindViewTap: function() {
@@ -42,13 +44,94 @@ Page({
         }
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  },//弹窗
+  btn: function () {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      showModal: true
+    })
+  },
+
+  // 禁止屏幕滚动
+  preventTouchMove: function () {
+  },
+
+  // 弹出层里面的弹窗
+  ok: function () {
+    this.setData({
+      showModal: false
     })
   }
+  ,
+  powerDrawer: function (e) {
+
+    var currentStatu = e.currentTarget.dataset.statu;
+
+    this.util(currentStatu)
+
+  },
+
+  util: function (currentStatu) {
+
+    var animation = wx.createAnimation({
+
+      duration: 200,
+
+      timingFunction: "linear",
+
+      delay: 0
+
+    });
+
+    this.animation = animation;
+
+    animation.opacity(0).rotateX(-100).step();
+
+    this.setData({
+
+      animationData: animation.export()
+
+    })
+
+    setTimeout(function () {
+
+      animation.opacity(1).rotateX(0).step();
+
+      this.setData({
+
+        animationData: animation
+
+      })
+
+      if (currentStatu == "close") {
+
+        this.setData(
+
+          {
+
+            showModalStatus: false
+
+          }
+
+        );
+
+      }
+
+    }.bind(this), 200)
+
+    if (currentStatu == "open") {
+
+      this.setData(
+
+        {
+
+          showModalStatus: true
+
+        }
+
+      );
+
+    }
+
+  }
+
 })
