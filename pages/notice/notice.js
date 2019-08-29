aace85c3c6b8acfa1821a8b69c928f355b8784d9
ljw777 <17347116335@163.con>
2019-08-29 09:30:52 +0800

    提交代码

--- pages/notice/notice.js
+++ pages/notice/notice.js
@@ -22,7 +22,7 @@ Page({
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true 
         })
       }
     } else {
