f90cf11e1da707e5a6af2f93ad6d8bd80f9559a8
ljw777 <17347116335@163.com>
2019-09-02 13:54:14 +0800

    提交代码

---
+++ pages/rankingList-two/rankingList.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
