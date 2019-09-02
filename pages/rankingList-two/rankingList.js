6c95efef81df753080a684987839d1df081b0d5f
ljw777 <17347116335@163.con>
2019-09-02 18:29:36 +0800

    提交diamante

--- pages/rankingList-two/rankingList.js
+++ pages/rankingList-two/rankingList.js
@@ -14,6 +14,7 @@ Page({
    * 组件的初始数据
    */
   data: {
+    showModal: false
   },
 
   /**
@@ -21,5 +22,64 @@ Page({
    */
   methods: {
 
+  },//分享弹窗
+  Btn: function () {
+    this.setData({
+      showModal: true
+    })
+  },
+
+  // 弹出层里面的弹窗
+  ok: function () {
+    this.setData({
+      showModal: false
+    })
+  },
+
+  // 禁止屏幕滚动
+  preventTouchMove: function () {
+  },
+  // 分享弹窗关闭
+  cancelShare: function () {
+    this.setData({
+      showModal: false
+    })
+  }
+  ,
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function (currentStatu) {
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    });
+    this.animation = animation;
+    animation.opacity(0).rotateX(-100).step();
+    this.setData({
+      animationData: animation.export()
+    })
+    setTimeout(function () {
+      animation.opacity(1).rotateX(0).step();
+      this.setData({
+        animationData: animation
+      })
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
   }
 })
