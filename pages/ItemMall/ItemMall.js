15779f1239fc4e56624437dbf731d781009cefb4
haolingjie <haoljsh@sinosoft.com.cn>
2019-09-05 01:35:03 +0800

    商城代码提交

--- pages/ItemMall/ItemMall.js
+++ pages/ItemMall/ItemMall.js
@@ -1,4 +1,25 @@
 // pages/ItemMall/ItemMall.js
+import {
+  post,
+  get
+} from '../../utils/http'
+import {
+  setValue,
+  redirectTo,
+  getValue,
+  showToast,
+  showModal,
+  navTo,
+  reLanchTo,
+  rnd,
+  goPage
+} from '../../utils/common';
+import util from '../../utils/util1'
+import {
+  config,
+  cmd
+} from '../../config'
+var that;
 Page({
 
   /**
@@ -12,7 +33,27 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var aUserInfo = getValue('aUserInfo');
+    this.setData({
+      aUserInfo: aUserInfo,
+    });
 
+    get('/wx/gameShop/' + config.appkey + '/getGameShopInfo', { 'openId': aUserInfo.openid, }).then(res => {
+      var bUserScoreVo = res.bUserScoreVo;
+      if (bUserScoreVo) {
+        this.setData({
+          bUserScoreVo: bUserScoreVo,
+        });
+      }
+      var bGoodList = res.bGoodList;
+      if (bGoodList) {
+        this.setData({
+          bGoodList: bGoodList,
+        });
+      }
+    });
+
+    that = this;
   },
 
   /**
