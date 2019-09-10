71721982787800bc0ca00163a3f20781e0ab2aaf
ljw777 <17347116335@163.com>
2019-09-10 17:49:42 +0800

    提交代码

--- pages/KangPK-two/KangPK-two.js
+++ pages/KangPK-two/KangPK-two.js
@@ -1,4 +1,4 @@
-// pages/MyDetails/MyDetails.js
+// pages/KangPK-two/KangPK-two.js
 import {
   post,
   get
@@ -26,8 +26,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showModal: false,
-    WhetherSign:2
+    getready: false,
+    problemList: false,
+    endSuccessful: false,
+    endFailure: false,
+    people: true,
   },
 
   /**
@@ -96,92 +99,9 @@ Page({
   goMyPointsDetails: function () {
     navTo('/pages/MyPointsDetails/MyPointsDetails');
   },
-  participateClick:function(){
+  participateClick: function () {
     this.setData({
       showModal: true
     })
   },
-
-  // 禁止屏幕滚动
-  preventTouchMove: function () {
-  },
-
-  // 弹出层里面的弹窗
-  ok: function () {
-    this.setData({
-      showModal: false
-    })
-  }
-  ,
-  powerDrawer: function (e) {
-
-    var currentStatu = e.currentTarget.dataset.statu;
-
-    this.util(currentStatu)
-
-  },
-
-  util: function (currentStatu) {
-
-    var animation = wx.createAnimation({
-
-      duration: 200,
-
-      timingFunction: "linear",
-
-      delay: 0
-
-    });
-
-    this.animation = animation;
-
-    animation.opacity(0).rotateX(-100).step();
-
-    this.setData({
-
-      animationData: animation.export()
-
-    })
-
-    setTimeout(function () {
-
-      animation.opacity(1).rotateX(0).step();
-
-      this.setData({
-
-        animationData: animation
-
-      })
-
-      if (currentStatu == "close") {
-
-        this.setData(
-
-          {
-
-            showModalStatus: false
-
-          }
-
-        );
-
-      }
-
-    }.bind(this), 200)
-
-    if (currentStatu == "open") {
-
-      this.setData(
-
-        {
-
-          showModalStatus: true
-
-        }
-
-      );
-
-    }
-
-  }
 })
--- pages/KangPKMatching/KangPKMatching.js
+++ pages/KangPKMatching/KangPKMatching.js
@@ -1,4 +1,5 @@
-// pages/MyDetails/MyDetails.js
+// pages/KangPKMatching/KangPKMatching.js
+
 import {
   post,
   get
@@ -26,8 +27,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showModal: false,
-    WhetherSign:2
+
   },
 
   /**
@@ -95,93 +95,6 @@ Page({
   },
   goMyPointsDetails: function () {
     navTo('/pages/MyPointsDetails/MyPointsDetails');
-  },
-  participateClick:function(){
-    this.setData({
-      showModal: true
-    })
-  },
-
-  // 禁止屏幕滚动
-  preventTouchMove: function () {
-  },
-
-  // 弹出层里面的弹窗
-  ok: function () {
-    this.setData({
-      showModal: false
-    })
   }
-  ,
-  powerDrawer: function (e) {
 
-    var currentStatu = e.currentTarget.dataset.statu;
-
-    this.util(currentStatu)
-
-  },
-
-  util: function (currentStatu) {
-
-    var animation = wx.createAnimation({
-
-      duration: 200,
-
-      timingFunction: "linear",
-
-      delay: 0
-
-    });
-
-    this.animation = animation;
-
-    animation.opacity(0).rotateX(-100).step();
-
-    this.setData({
-
-      animationData: animation.export()
-
-    })
-
-    setTimeout(function () {
-
-      animation.opacity(1).rotateX(0).step();
-
-      this.setData({
-
-        animationData: animation
-
-      })
-
-      if (currentStatu == "close") {
-
-        this.setData(
-
-          {
-
-            showModalStatus: false
-
-          }
-
-        );
-
-      }
-
-    }.bind(this), 200)
-
-    if (currentStatu == "open") {
-
-      this.setData(
-
-        {
-
-          showModalStatus: true
-
-        }
-
-      );
-
-    }
-
-  }
 })
